6adf159f3d73dbf833de6ae8e97009d431bfe4c7
Vlad Suhomlinov <>
2021-05-17 22:42:50 +0300

    feat: add base PasswordChecker

--- Sources/PasswordChecker/PasswordChecker.swift
+++ Sources/PasswordChecker/PasswordChecker.swift
@@ -1,28 +1,63 @@
 import JavaScriptCore
 
-enum PasswordChecker {
-    
-    static func getPasswordScore(_ password: String) throws -> Int32 {
+public enum PasswordCheckerError: Error {
+    case unableToCreateJSContext
+    case unableToGetScript
+    case unableToParseResult
+}
+
+// For more, available properties - https://github.com/dropbox/zxcvbn#usage
+public struct PasswordInfo {
+    let score: Int32
+}
+
+public enum PasswordChecker {
+
+    public static func getPasswordScore(_ password: String,
+                                        userInputs: [String] = []) -> Result<PasswordInfo, PasswordCheckerError> {
+
         guard let jsContext = JSContext() else {
-            throw NSError()
-        }
-        
-        // Load zxcvbn into JSContext
-        guard let zxcvbnPath = Bundle.module.path(forResource: "zxcvbn", ofType: "js") else {
-            throw NSError()
+            return .failure(PasswordCheckerError.unableToCreateJSContext)
         }
 
-        let zxcvbnJS = try String(contentsOfFile: zxcvbnPath, encoding: String.Encoding.utf8)
-        jsContext.evaluateScript(zxcvbnJS)
-        
-        // Set password to context, evaluate script and get result
-        jsContext.setObject(password, forKeyedSubscript: "password" as NSString)
-
-        guard let result = jsContext.evaluateScript("zxcvbn(password)"),
-              let value = result.objectForKeyedSubscript("score") else {
-            throw NSError()
+        guard let passwordCheckerPath = Bundle.module.path(forResource: JSScript.scriptName, ofType: JSScript.scriptType),
+              let passwordCheckerJS = try? String(contentsOfFile: passwordCheckerPath, encoding: String.Encoding.utf8) else {
+            
+            return .failure(PasswordCheckerError.unableToGetScript)
         }
-        
-        return value.toInt32()
+
+        jsContext.evaluateScript(passwordCheckerJS)
+
+        jsContext.setObject(password, forKeyedSubscript: JSKey.password.asNSString)
+        jsContext.setObject(userInputs, forKeyedSubscript: JSKey.userInputs.asNSString)
+
+        guard let result = jsContext.evaluateScript(JSScript.scriptToEvaluate),
+              let score = result.objectForKeyedSubscript(JSKey.score.rawValue)?.toInt32() else {
+            
+            return .failure(PasswordCheckerError.unableToParseResult)
+        }
+
+        let passwordInfo = PasswordInfo(score: score)
+
+        return .success(passwordInfo)
+    }
+}
+
+private extension PasswordChecker {
+
+    enum JSKey: String, RawRepresentable {
+        case password
+        case userInputs = "user_inputs"
+        case score
+
+        var asNSString: NSString {
+            rawValue as NSString
+        }
+    }
+
+    enum JSScript {
+        static let scriptName = "zxcvbn"
+        static let scriptType = "js"
+        static let scriptToEvaluate = "zxcvbn(password, user_inputs)"
     }
 }
